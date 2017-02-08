b7a839e7a797ab6b1f1d1bcbb9ee6381832c3772
mknezic <mknezic@croz.net>
2017-02-08 15:18:28 +0100

    check node suport in vars

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -2,4 +2,17 @@ def call(String name = 'human') {
     // Any valid steps can be called from this code, just like in other
     // Scripted Pipeline
     echo "Hello, ${name}."
+
+    stage "test node call"
+    doNodeJob("jobBlaX")
+
+    stage "something"
+    echo "do something"
+
+}
+
+def doNodeJob(String jobName = 'jobx') {
+    node() {
+        echo "job name is " + jobName
+    }
 }
